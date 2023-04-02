c3a4784af9a759071d6d16967552d5f9f6932848
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-04-03 04:43:28 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo
+while :; do
+    rand_name=VPS-Bizfly
+    rand_num=$((RANDOM % 9999 + 1))
+    "Ví CLI"
+    ./astrominer -w dero1qy9qn24xrqkakc0ftnzv0zlcqztlz973f2ndjsurgnu8cez7rag4gqg5gxje9.$rand_name-$rand_num -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -m 8;
+    sleep 5;
+done
